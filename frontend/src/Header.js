03500e0a64684f7edb665bf4766e0f84d739819f
Personal Github Profile <elle.dunbar@gmail.com>
2021-10-25 22:54:39 +1030

    Working range slider

--- frontend/src/Header.js
+++ frontend/src/Header.js
@@ -1,16 +1,64 @@
 // NPM MODULES
 import React, { Component, Fragment } from 'react';
+import { Range } from 'react-range';
+
+import IconSearch from './assets/icons8-search.svg';
+
 
 export default class Header extends Component {
     constructor(props) {
         super(props);
-        this.state = {}
+        this.state = {
+            values: [1970,2015]
+        }
     }
 
+
+
     render() {
+
         return(
             <Fragment>
-                <input value={this.props.searchValue} name="searchValue" onChange={(e) => this.props.handleSearchChange(e) }/>
+                <div className="input-search">
+                    <img src={IconSearch} className="Icon-Search" alt="Search Icon" />
+                    <input value={this.props.searchValue} name="searchValue" onChange={(e) => this.props.handleSearchChange(e) }/>
+                </div>
+                <div className="range-search">
+                    <Range
+                        step={0.1}
+                        min={1950}
+                        max={2022}
+                        values={this.state.values}
+                        onChange={(values) => this.setState({ values })}
+                        renderTrack={({ props, children }) => (
+                            <div
+                                {...props}
+                                style={{
+                                    ...props.style,
+                                    height: '6px',
+                                    width: '100%',
+                                    backgroundColor: '#ccc'
+                                }}
+                            >
+                                {children}
+                            </div>
+                        )}
+                        renderThumb={({ props }) => (
+                            <div
+                                {...props}
+                                style={{
+                                    ...props.style,
+                                    height: '42px',
+                                    width: '42px',
+                                    backgroundColor: '#999'
+                                }}
+                            />
+                        )}
+                    />
+                </div>
+                <div className="option-search">
+
+                </div>
             </Fragment>
         );
     }
